1785a1686a5479b0fb216454ff11db73c6c49f7e
seb1gonzalez <sebastian95gonzalez@gmail.com>
2018-12-17 14:38:11 -0700

    first push into branch:dev

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CTIS - UTC</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<div>
+
+
+<!-- Navigation -->
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+        </div>
+        <?php include('navbar.php'); ?>
+        </div>
+        <!-- /.container -->
+    </nav>
+
+
+<!-- Page Content -->
+    <hr>
+    <!-- Image Header -->
+    <div class="row">
+        <div class="img-responsive text-center">
+            <img src="tlc.png" alt="TLC" >
+        </div>
+    </div>
+
+    <!-- /.row -->
+    <hr>
+
+    <div class="row">
+        <h2 style="text-align:center;">Transportation Leadership Council</h2>
+        <div  class="container-fluid">
+        <p>The purpose of the TLC Student Chapter is to promote the collaboration of multi-disciplinary transportation
+            professionals and UTEP students by fostering the development of leadership, teamwork, networking, and
+            research skills through the activities conducted by the Chapter.
+
+            TLC Activities:
+        <ul>
+            <li>Present research work in conferences</li>
+            <li>Conduct monthly meetings with professional presenters from the transportation community</li>
+            <li>Participate in community service events</li>
+            <li>Promote networking opportunities</li>
+        </ul>
+
+        Membership is open to all students with interest in the transportation field. No membership dues required.<br>
+        Call for Officers! If you are interested in serving as an officer for TLC please contact Benjamin Arras
+        (barras@miners.utep.edu) or Dr. Imad Abdallah(emadn@utep.edu).
+        <hr>
+        </div>
+        <div>
+            <h2 style="text-align:center;">Officers</h2>
+            <ul>
+                <li>
+                    <b>President</b> - Benjamin Arras
+                </li>
+                <li>
+                    <b>Vice President</b> - Mayra Reyna
+                </li>
+                <li>
+                    <b>Secretary</b> - Margarita Ordaz
+                </li>
+                <li>
+                    <b>Treasurer</b> - Angelica Torres
+                </li>
+                <li>
+                    <b>Webmaster & Historian</b> - Sebastian Gonzalez
+                </li>
+            </ul>
+        </div>
+        <hr>
+ <div>
+     <ul class="nav nav-pills">
+         <li role="presentation" class="active"><a href="#">Home</a></li>
+         <li role="presentation"><a href="#">Profile</a></li>
+         <li role="presentation"><a href="#">Messages</a></li>
+     </ul>
+<!--            <ul class="nav nav-tabs">-->
+<!--                <li role="presentation">Documents Transportation Leadership Council (TLC) --->
+<!--                    <a href="doc/Transportation Leadership Council (TLC) -Presentation-2015-09-10-R3.pptx">Presentation</a>-->
+<!--                    - 2015-09-10-R3-->
+<!--                </li>-->
+<!--                <li role="presentation"><a href="TLC%20Fund%20Request%20Form.pdf" target="_blank">Fund Request Form PDF</a></li>-->
+<!---->
+<!--                <li role="presentation"><a href="#">Profile</a></li>-->
+<!--                <li role="presentation"><a href="#">Messages</a></li>-->
+<!--            </ul>-->
+
+
+
+        </div>
+        <hr>
+
+    </div>
+
+<div class="container">
+
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+            <li data-target="#myCarousel" data-slide-to="5"></li>
+            <li data-target="#myCarousel" data-slide-to="6"></li>
+            <li data-target="#myCarousel" data-slide-to="7"></li>
+            <li data-target="#myCarousel" data-slide-to="8"></li>
+            <li data-target="#myCarousel" data-slide-to="9"></li>
+            <li data-target="#myCarousel" data-slide-to="10"></li>
+            <li data-target="#myCarousel" data-slide-to="11"></li>
+            <li data-target="#myCarousel" data-slide-to="12"></li>
+            <li data-target="#myCarousel" data-slide-to="13"></li>
+        </ol>
+
+
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="img/new_officers_1.jpg" alt="New Officers"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC New Officers</h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/new_officers_2.jpg" alt="New Officers"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC New Officers</h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/sundt_1.jpg" alt="SUNDT"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC SUNDT 2018 </h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/sundt_2.jpg" alt="SUNDT"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC SUNDT 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/sundt_3.jpg" alt="SUNDT"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC SUNDT 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/sundt_4.jpg" alt="SUNDT"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC SUNDT 2018 </h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_1.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_2.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_3.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_4.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_5.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" height="50%" width="50%">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_6.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" class="img-fluid" width="50%"
+                     height="auto">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_7.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" class="img-fluid" width="50%"
+                     height="auto">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="img/gen_meet_fall18_8.jpg" alt="First meeting"
+                     style="display: block; margin-left: auto; margin-right: auto;" class="img-fluid" width="50%"
+                     height="auto">
+                <div class="carousel-caption">
+                    <h2>TLC First General Meeting Fall 2018 </h2>
+                </div>
+            </div>
+        </div>
+
+
+
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    </div>
+ </div>
+
+<hr>
+    <img id="footerpic" src="../../footer.png">
+    <!-- Footer -->
+    <?php include('../../footer.php'); ?>
+
+
+
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/tabs.js"></script>
+<script type="text/javascript">
+    $('#myTab').tabCollapse();
+    $('#myTab2').tabCollapse();
+</script>
+</body>
+
+</html>
